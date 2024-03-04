5bd47c9e9f1419e754191b323df9a6ce1d331629
SauravKumarLal <sauravkrlal@gmail.com>
2024-03-05 01:25:52 +0530

    tags & course controllers done

---
+++ controllers/Course.js
@@ -0,0 +1,115 @@
+//models import
+const Course = require("../models/Course");
+const Tag = require("../models/tags");
+const User = require("../models/User")
+const {uploadImageToCloudinary} = require("../utils/imageUploader");
+
+//createCourse handler function
+exports.createCourse = async (req, res) => {
+    try{
+        //fetch data from body
+        const {courseName, courseDescription, whatYouWillLearn, price, tag} = req.body; //ye sab req ki body mei aarha
+
+        //get thumbnail
+        const thumbnail = get.files.thumbnailImage;
+
+        //validation
+        if(!courseName || !courseDescription || !whatYouWillLearn || !price || !thumbnail || !tag){
+            return res.status(400).json({
+                success: false,
+                message: 'All fields are required!'
+            });
+        }
+
+        //check for instructor
+        const userId = req.user.id;
+        const instructorDetails = await User.findById(userId);
+        console.log('Instructor Deatils: ', instructorDetails);
+        
+        //TODO: verify that userId & instructorDetails._id are same or different?
+        if(!instructorDetails){
+            return res.status(404).json({
+                success: false,
+                message: 'Instructor details not found!'
+            })
+        }
+
+        //check given tag is valid or not
+        const tagDetails = await Tag.findById(tag);
+        if(!tagDetails){
+            return res.status(404).json({
+                success: false,
+                message: 'Tag details not found!'
+            })
+        }
+
+        //Upload image to cloudinary
+        const thumbnailImage = await uploadImageToCloudinary(thumbnail, process.env.FOLDER_NAME);
+
+        //create an entry for new course
+        const newCourse = await Course.create({
+            courseName,
+            courseDescription,
+            instructor: instructorDetails._id, //bcuz instructor is an id in schema
+            whatYouWillLearn: whatYouWillLearn,
+            price,
+            tag: tagDetails._id,
+            thumbnail: thumbnailImage.secure_url
+        })
+
+        //add the new course to the user schema of Instructor
+        await User.findByIdAndUpdate(
+            {_id: instructorDetails._id},
+            {
+                $push: {
+                    courses: newCourse._id
+                }
+            },
+            {new: true},
+        );
+
+        //update the TAG ka schema
+        
+        //return response
+        return res.status(200).json({
+            success: true,
+            message: 'Course creted successfully',
+            data:newCourse
+        });
+    }
+    catch(error){
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Failed to create course!',
+            error: error.message
+        })
+    }
+};
+
+
+//getAllCourses handler function
+exports.showAllCourses = async (req, res) => {
+    try{
+        const allCourses = await Course.find({}, {courseName: true, 
+                                                    price: true,
+                                                    thumbnail: true,
+                                                    instructor: true,
+                                                    ratingAndReviews: true,
+                                                    studentsEnrolled: true})
+                                                    .populate("instructor")
+                                                    .exec();
+        return res.status(200).json({
+            success: true,
+            message: 'Data for all courses fetched successfully',
+            data: allCourses
+        })
+    }
+    catch(error){
+        return res.status(500).json({
+            success: false,
+            message: 'Cannot fetch course data',
+            error: error.message
+        })
+    }
+}
---
+++ controllers/Tags.js
@@ -0,0 +1,54 @@
+const Tag = require('../models/tags');
+
+//craeteTag ka handler fnn..
+
+exports.createTag = async(req, res) => {
+    try{
+        //fetch data from request ki body
+        const {name, description} = req.body;
+
+        if(!name || !description){
+            return res.status(400).json({
+                success: false,
+                message: "All data required!"
+            })
+        }
+        //create entry in db
+        const tagDetails = await Tag.create({
+            name: name,
+            description: description
+        });
+        console.log(tagDetails);
+
+        //return response
+        return res.status(200).json({
+            success: true,
+            message: "Tag created successfully!"
+        })
+    }
+    catch(error){
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+};
+
+//getAlltags handler function..
+exports.showAlltags = async (req, res) => {
+    try{
+        const allTags = await Tag.find({}, {name: true, description:true}); //kisi criteria ya attribute k basis pe find nhi krna chahta, jo entry hai db mei le aana bas 1 cheez make sure krna saare entry ke andar name n description hona chahiye
+        res.status(200).json({
+            success: true,
+            message: "All tags return successfully",
+            allTags
+        })
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+};
